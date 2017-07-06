4cee47767b219fba659650d8c510a002117df034
zulquarnain <zulquarnain@gaditek.com>
2017-07-06 09:59:36 +0100

    fixed pool exhaust issue

--- DHCP.py
+++ DHCP.py
@@ -15,8 +15,10 @@ class DHCP:
             t = randint(0, 255)
             self.transactionID += struct.pack('!B', t) 
 
-    def DiscoverPacket(self):
-        packet = b''
+    def DiscoverPacket(self, _client_address, hw_address):
+	_hw = hw_address.split(':')
+	mac = ''.join(_hw)        
+	packet = b''
         packet += b'\x01'   #Message type: Boot Request (1)
         packet += b'\x01'   #Hardware type: Ethernet
         packet += b'\x06'   #Hardware address length: 6
@@ -24,18 +26,17 @@ class DHCP:
         packet += self.transactionID       #Transaction ID
         packet += b'\x00\x00'    #Seconds elapsed: 0
         packet += b'\x80\x00'   #Bootp flags: 0x8000 (Broadcast) + reserved flags
-        packet += b'\x00\x00\x00\x00'   #Client IP address: 0.0.0.0
-        packet += b'\x00\x00\x00\x00'   #Your (client) IP address: 0.0.0.0
+        packet += self.address2bin(_client_address, '.')   #Client IP address: 0.0.0.0
+        packet += self.address2bin(_client_address, '.')   #Your (client) IP address: 0.0.0.0
         packet += b'\x00\x00\x00\x00'   #Next server IP address: 0.0.0.0
         packet += b'\x00\x00\x00\x00'   #Relay agent IP address: 0.0.0.0
-        packet += b'\x00\x26\x9e\x04\x1e\x9b'   #Client MAC address: 00:26:9e:04:1e:9b
+        packet += self.getMacInBytes(  _hw )   #Client MAC address: 00:26:9e:04:1e:9b
         packet += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'   #Client hardware address padding: 00000000000000000000
         packet += b'\x00' * 67  #Server host name not given
         packet += b'\x00' * 125 #Boot file name not given
         packet += b'\x63\x82\x53\x63'   #Magic cookie: DHCP
         packet += b'\x35\x01\x01'   #Option: (t=53,l=1) DHCP Message Type = DHCP Discover
         #packet += b'\x3d\x06\x00\x26\x9e\x04\x1e\x9b'   #Option: (t=61,l=6) Client identifier
-        packet += b'\x3d\x06' + b'\x00\x26\x9e\x04\x1e\x9b'
         packet += b'\x37\x01\x36'   #Option: (t=55,l=3) Parameter Request List
         packet += b'\xff'   #End Option
         return packet
@@ -112,7 +113,7 @@ if __name__ == '__main__':
 	sys.exit(os.EX_SOFTWARE)    
  
     dhcp = DHCP()	
-    dhcps.sendto(dhcp.DiscoverPacket(), (args.dhcp_broadcast_ip, 67))
+    dhcps.sendto(dhcp.DiscoverPacket(args.client_ip, args.mac), (args.dhcp_broadcast_ip, 67))
     dhcps.settimeout(DHCP_TIMEOUT)
     print('DHCP Discover......................, Waiting for 10sec')
     try:
